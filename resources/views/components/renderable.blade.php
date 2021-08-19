bc78aac9ae0f2515698fcda10635df38e2dc65f8
Cole <cole.shirley@artificertech.com>
2021-08-19 15:57:33 -0600

    added slot capabilites to the renderable component

--- resources/views/components/renderable.blade.php
+++ resources/views/components/renderable.blade.php
@@ -1,3 +1,16 @@
 @props(['renderable'])
 
-{{ $renderable->render()->with($attributes->getAttributes())->with('attributes', $attributes)->with($renderable->variableName(), $renderable) }}
+@php
+$renderableData = ['attributes' => $attributes, $renderable->variableName() => $renderable];
+
+foreach ($__laravel_slots as $key => $value) {
+    if ($key == '__default') {
+        $key = 'slot';
+    }
+    $renderableData[$key] = $value;
+}
+@endphp
+
+<div>
+    {{ $renderable->render()->with($attributes->getAttributes())->with($renderableData) }}
+</div>
